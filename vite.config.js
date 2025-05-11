89def93637c0f720f67164f8cbc335362d788d29
CreigBoyd <creigb1@gmail.com>
2025-05-10 22:53:46 -0400

    tghth
    thththt

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/GreenT/',
+  base: 'https://creigboyd.github.io/GreenT/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
