06c7337a59f81b0cff03156e95a12be7649459ac
CreigBoyd <creigb1@gmail.com>
2025-05-11 06:43:57 -0400

    hundo commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<FreenT>/', // Replace <your-repo-name> with your actual GitHub repository name
+  base: '/GreenT/', // Replace <your-repo-name> with your actual GitHub repository name
 });
