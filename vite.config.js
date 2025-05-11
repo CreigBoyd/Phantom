0913fb8ff9b0b2bb2f4c51ccb4273015b2360799
CreigBoyd <creigb1@gmail.com>
2025-05-10 23:43:01 -0400

    Initial comyytyymit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   base: '/',
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['mixed-decls'],
-      },
-    },
-  },
-})
+  base: '/<FreenT>/', // Replace <your-repo-name> with your actual GitHub repository name
+});
