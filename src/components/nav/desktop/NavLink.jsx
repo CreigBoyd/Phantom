3494b573d2b5511c82a18dd83b2ad7c84c1dea6a
CreigBoyd <creigb1@gmail.com>
2025-05-11 13:41:02 -0400

    ved
    ved

--- src/components/nav/desktop/NavLink.jsx
+++ src/components/nav/desktop/NavLink.jsx
@@ -1,26 +1,43 @@
-import "./NavLink.scss"
-import React from 'react'
-import MenuItem from "/src/components/generic/MenuItem.jsx"
-import {useUtils} from "/src/helpers/utils.js"
-import SensitiveButton from "/src/components/generic/SensitiveButton.jsx"
+import "./NavLink.scss";
+import React from 'react';
+import MenuItem from "/src/components/generic/MenuItem.jsx";
+import { useUtils } from "/src/helpers/utils.js";
+import SensitiveButton from "/src/components/generic/SensitiveButton.jsx";
+import useSound from 'use-sound';
+import soundURL from '/sounds/smash.mp3'; // Adjust the import path to your sound file
 
 function NavLink({ shrink, label, icon, size, className, rounded, selected, disabled, onClick, tooltip }) {
-    const utils = useUtils()
+    const utils = useUtils();
+
+    // Initialize the sound
+    const [play] = useSound(soundURL, { volume: 0.5 });
+
+    // Handle the button click
+    const handleClick = () => {
+        if (onClick) {
+            onClick(); // Execute the passed onClick function
+        }
+        play(); // Play the sound
+    };
 
     return (
-        <SensitiveButton className={`nav-link ${utils.strIf(rounded, `nav-link-rounded`)}`}
-                         disabled={disabled}
-                         onClick={onClick}>
-            <MenuItem shrink={shrink}
-                      label={label}
-                      icon={icon}
-                      hoverAnimation={true}
-                      size={size}
-                      tooltip={tooltip}
-                      selected={selected}
-                      className={className}/>
+        <SensitiveButton 
+            className={`nav-link ${utils.strIf(rounded, `nav-link-rounded`)}`}
+            disabled={disabled}
+            onClick={handleClick} // Update to use the new handleClick function
+        >
+            <MenuItem 
+                shrink={shrink}
+                label={label}
+                icon={icon}
+                hoverAnimation={true}
+                size={size}
+                tooltip={tooltip}
+                selected={selected}
+                className={className}
+            />
         </SensitiveButton>
-    )
+    );
 }
 
-export default NavLink
+export default NavLink;
