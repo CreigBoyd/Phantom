b01ca47b7ea195678d299e1c03578be2a6fc2808
CreigBoyd <creigb1@gmail.com>
2025-05-20 21:12:37 -0400

    adding poohpss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,29 @@
-import "/src/styles/app.scss"
-import React, {useEffect, useState} from 'react'
-import {useData} from "/src/providers/DataProvider.jsx"
-import Portfolio from "/src/components/Portfolio.jsx"
-import {AnimatedCursor} from "/src/components/feedbacks/AnimatedCursor"
-import ActivitySpinner from "/src/components/feedbacks/ActivitySpinner.jsx"
-import ImageCache from "/src/components/generic/ImageCache.jsx"
-import YoutubeModal from "/src/components/modals/YoutubeModal.jsx"
-import GalleryModal from "/src/components/modals/GalleryModal.jsx"
-import Notifications from "/src/components/feedbacks/Notifications.jsx"
-import ConfirmationWindow from "/src/components/modals/ConfirmationWindow.jsx"
-import {useFeedbacks} from "/src/providers/FeedbacksProvider.jsx"
+import "/src/styles/app.scss";
+import React from 'react';
+import { useData } from "/src/providers/DataProvider.jsx";
+import Portfolio from "/src/components/Portfolio.jsx";
+import { AnimatedCursor } from "/src/components/feedbacks/AnimatedCursor";
+import ActivitySpinner from "/src/components/feedbacks/ActivitySpinner.jsx";
+import ImageCache from "/src/components/generic/ImageCache.jsx";
+import YoutubeModal from "/src/components/modals/YoutubeModal.jsx";
+import GalleryModal from "/src/components/modals/GalleryModal.jsx";
+import Notifications from "/src/components/feedbacks/Notifications.jsx";
+import ConfirmationWindow from "/src/components/modals/ConfirmationWindow.jsx";
+import { useFeedbacks } from "/src/providers/FeedbacksProvider.jsx";
+
 
 function App() {
-    const {listImagesForCache} = useData()
-
-    const imageList = listImagesForCache()
+    const { listImagesForCache } = useData();
+    const imageList = listImagesForCache();
 
     return (
         <div className={`app-wrapper`}>
-            <AppFeedbacks/>
-            <ImageCache urls={imageList}/>
-            <Portfolio/>
+           
+            <AppFeedbacks />
+            <ImageCache urls={imageList} />
+            <Portfolio />
         </div>
-    )
+    );
 }
 
 function AppFeedbacks() {
@@ -39,46 +40,50 @@ function AppFeedbacks() {
         hideGallery,
         pendingConfirmation,
         hideConfirmationDialog
-    } = useFeedbacks()
+    } = useFeedbacks();
 
-
-    const spinnerActivities = listSpinnerActivities()
-    const animatedCursorEnabled = isAnimatedCursorEnabled()
-    const animatedCursorActive = isAnimatedCursorActive()
-    const modalOpen = isModalOpen()
+    const spinnerActivities = listSpinnerActivities();
+    const animatedCursorEnabled = isAnimatedCursorEnabled();
+    const animatedCursorActive = isAnimatedCursorActive();
+    const modalOpen = isModalOpen();
 
     return (
         <>
             {spinnerActivities && (
-                <ActivitySpinner activities={spinnerActivities}/>
+                <ActivitySpinner activities={spinnerActivities} />
             )}
 
-            {isAnimatedCursorEnabled() && (
-                <AnimatedCursor enabled={animatedCursorEnabled}
-                                active={animatedCursorActive}
-                                modalOpen={modalOpen}/>
+            {animatedCursorEnabled && (
+                <AnimatedCursor
+                    enabled={animatedCursorEnabled}
+                    active={animatedCursorActive}
+                    modalOpen={modalOpen}
+                />
             )}
 
             {displayingNotification && (
-                <Notifications displayingNotification={displayingNotification}
-                               killNotification={killNotification}/>
+                <Notifications
+                    displayingNotification={displayingNotification}
+                    killNotification={killNotification}
+                />
             )}
 
+            <YoutubeModal
+                displayingYoutubeVideo={displayingYoutubeVideo}
+                hideYoutubeVideo={hideYoutubeVideo}
+            />
 
-            <YoutubeModal   displayingYoutubeVideo={displayingYoutubeVideo}
-                            hideYoutubeVideo={hideYoutubeVideo}/>
+            <GalleryModal
+                displayingGallery={displayingGallery}
+                hideGallery={hideGallery}
+            />
 
-
-
-            <GalleryModal   displayingGallery={displayingGallery}
-                            hideGallery={hideGallery}/>
-
-
-
-            <ConfirmationWindow pendingConfirmation={pendingConfirmation}
-                                hideConfirmationDialog={hideConfirmationDialog}/>
+            <ConfirmationWindow
+                pendingConfirmation={pendingConfirmation}
+                hideConfirmationDialog={hideConfirmationDialog}
+            />
         </>
-    )
+    );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GreenT/', // Replace <your-repo-name> with your actual GitHub repository name
+  base: '/603Phantom/', // Replace <your-repo-name> with your actual GitHub repository name
 });
