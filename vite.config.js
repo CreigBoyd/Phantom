81bee615f723d2e5f20b8b69ea43189faf194666
CreigBoyd <creigb1@gmail.com>
2025-05-20 21:48:21 -0400

    adding poohpzyzz

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/603Phantom/', // Replace <your-repo-name> with your actual GitHub repository name
+  base: '/Phantom/', // Replace <your-repo-name> with your actual GitHub repository name
 });
